c72593fbc1973740953bef731c4d130bfb0f8443
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-02 14:50:14 -0800

    Update welcome.blade.php

--- Backend/resources/views/welcome.blade.php
+++ Backend/resources/views/welcome.blade.php
@@ -21,9 +21,9 @@
 	<title>Databae</title>
 </head>
 
+	@section('navbar')
 <body>
 	<!-- Navbar -->
-@section('navbar')
 	<!-------------------- Image Slideshow -------------------->
 	<div class="container">
 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
