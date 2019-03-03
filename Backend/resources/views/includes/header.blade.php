b702924fabc5092c728de5f4806f29ba98b759c9
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-02 17:03:49 -0800

    Update header.blade.php

--- Backend/resources/views/includes/header.blade.php
+++ Backend/resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<a class="navbar-brand" href="/">DataBae Recipes</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
