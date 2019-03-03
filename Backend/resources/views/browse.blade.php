37401f723823c6d42ebcc82943d542d33965726a
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-02 17:11:54 -0800

    Create browse.blade.php

---
+++ Backend/resources/views/browse.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')	
+<div class="container" style="margin-top: 50px">
+		<div class="card-group">
+			<div class="card">
+				<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Mac 'n Cheese</h5>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Chicken Soup</h5>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star"></span>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Generic Salad</h5>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star"></span>
+					<span class="fa fa-star"></span>
+					<p class="card-text">Upload an image of your recipe here.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+		</div>
+		<!-- End of Card Group -->
+		<div class="card-group">
+			<div class="card">
+				<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Mac 'n Cheese</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Chicken Soup</h5>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Generic Salad</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
