ec3f27a31f877fa4f755a8ede9d98de8be71cde7
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-02 16:11:13 -0800

    Update welcome.blade.php

--- Backend/resources/views/welcome.blade.php
+++ Backend/resources/views/welcome.blade.php
@@ -3,105 +3,6 @@
 @section('content')
 <!-------------------- Image Slideshow -------------------->
 <div class="container">
-	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-		<ol class="carousel-indicators">
-			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-		</ol>
-		<div class="carousel-inner">
-			<div class="carousel-item active">
-				<img class="d-block w-100" src="img/burg.jpg" alt="First slide">
-				<div class="carousel-caption d-none d-md-block">
-					<h1>Spicy Bacon Burger</h1>
-					<p>Something with a little heat!</p>
-=======
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-
-	<!-- Other CSS -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/signin.css">
-
-	<!-- Favicon -->
-	<link rel="shortcut icon" href="img/favicon2.ico" />
-
-	<title>Databae</title>
-</head>
-
-<body>
-	<!-- Navbar -->
-	<div class="container-fluid">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="/">DataBae Recipes</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="#">Recipes</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">User Recipes</a>
-					</li>
-				</ul>
-				<form class="form-inline ml-auto mr-auto">
-					<div class="btn-group">
-						<input class="form-control" type="text" placeholder="Search Recipes..." aria-label="Search" style="width: 500px">
-						<button class="btn btn-default" type="submit">
-							<img src="img/search_icon.png" alt="Smiley face" height="20" width="20">
-						</button>
-					</div>
-				</form>
-			</div>
-			<!-- Sign in Button -->
-			<ul class="navbar-nav ml-auto">
-				<!-- Authentication Links -->
-				@guest
-				<div class="btn-group">
-						<a class="btn btn-dark navbar-btn login-btn" href="{{ route('login') }}">{{ __('Login') }}</a>
-						@if (Route::has('register'))
-						<a class="btn btn-danger navbar-btn login-btn" href="{{ route('register') }}">{{ __('Register') }}</a>
-						@endif
-				</div>
-				@else
-				<a href="/uploadrecipes" class="btn btn-secondary navbar-btn login-btn" style="margin-right: 50px">Upload a Recipe</a>
-				<li class="nav-item dropdown">
-
-					<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-					<img class="rounded-circle" width="30px" height="30px" src="/storage/avatars/{{ Auth::user()->avatar }}" /></img> {{ Auth::user()->username }} <span class="caret"></span>
-					</a>
-
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="/profile">
-                                        	Profile
-                                    		</a>
-
-						<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-							{{ __('Logout') }}
-						</a>
-
-						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-							@csrf
-						</form>
-					</div>
-				</li>
-				@endguest
-			</ul>
-		</nav>
-	</div>
-	<!-------------------- Image Slideshow -------------------->
-	<div class="container">
 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 			<ol class="carousel-indicators">
 				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -115,103 +16,101 @@
 						<h1>Spicy Bacon Burger</h1>
 						<p>Something with a little heat!</p>
 					</div>
->>>>>>> parent of ffdcbe5... Update welcome.blade.php
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="img/cookies.jpeg" alt="Second slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h1>10 Holiday Treats</h1>
+						<p>Sweeten up the holidays with these holiday treats.</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="img/chorizo.jpg" alt="Third slide">
+					<div class="carousel-caption d-none d-md-block">
+						<h1>Chorizo Pasta</h1>
+						<p>This is actual shit tbh. Don't make it.</p>
+					</div>
 				</div>
 			</div>
-			<div class="carousel-item">
-				<img class="d-block w-100" src="img/cookies.jpeg" alt="Second slide">
-				<div class="carousel-caption d-none d-md-block">
-					<h1>10 Holiday Treats</h1>
-					<p>Sweeten up the holidays with these holiday treats.</p>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+	<!-- Bottom Content	-->
+	<div class="container" style="margin-top: 50px">
+		<div class="card-group">
+			<div class="card">
+				<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Mac 'n Cheese</h5>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
 			</div>
-			<div class="carousel-item">
-				<img class="d-block w-100" src="img/chorizo.jpg" alt="Third slide">
-				<div class="carousel-caption d-none d-md-block">
-					<h1>Chorizo Pasta</h1>
-					<p>This is actual shit tbh. Don't make it.</p>
+			<div class="card">
+				<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Chicken Soup</h5>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star"></span>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			</div>
+			<div class="card">
+				<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Generic Salad</h5>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star checked"></span>
+					<span class="fa fa-star"></span>
+					<span class="fa fa-star"></span>
+					<p class="card-text">Upload an image of your recipe here.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
 			</div>
 		</div>
-		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
-</div>
-<!-- Bottom Content	-->
-<div class="container" style="margin-top: 50px">
-	<div class="card-group">
-		<div class="card">
-			<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Mac 'n Cheese</h5>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		<!-- End of Card Group -->
+		<div class="card-group">
+			<div class="card">
+				<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Mac 'n Cheese</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
 			</div>
-		</div>
-		<div class="card">
-			<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Chicken Soup</h5>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star"></span>
-				<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			<div class="card">
+				<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Chicken Soup</h5>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
 			</div>
-		</div>
-		<div class="card">
-			<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Generic Salad</h5>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star checked"></span>
-				<span class="fa fa-star"></span>
-				<span class="fa fa-star"></span>
-				<p class="card-text">Upload an image of your recipe here.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+			<div class="card">
+				<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Generic Salad</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
 			</div>
 		</div>
 	</div>
-	<!-- End of Card Group -->
-	<div class="card-group">
-		<div class="card">
-			<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Mac 'n Cheese</h5>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-			</div>
-		</div>
-		<div class="card">
-			<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Chicken Soup</h5>
-				<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-			</div>
-		</div>
-		<div class="card">
-			<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Generic Salad</h5>
-				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-				<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-			</div>
-		</div>
-	</div>
-</div>
 @stop
