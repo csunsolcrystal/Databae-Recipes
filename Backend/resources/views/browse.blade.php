6d7ebd4c6edb4cec2e03896200e447225e41eaee
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-02 17:31:04 -0800

    Update browse.blade.php

--- Backend/resources/views/browse.blade.php
+++ Backend/resources/views/browse.blade.php
@@ -4,14 +4,8 @@
 <div class="container" style="margin-top: 50px">
 		<div class="card-group">
 			<div class="card">
-				<img class="card-img-top" src="img/mac.jpg" alt="Card image cap">
 				<div class="card-body">
-					<h5 class="card-title">Mac 'n Cheese</h5>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
+					<h5 class="card-title">Italian</h5>
 					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
@@ -20,11 +14,6 @@
 				<img class="card-img-top" src="img/soup.jpg" alt="Card image cap">
 				<div class="card-body">
 					<h5 class="card-title">Chicken Soup</h5>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star"></span>
 					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
@@ -33,11 +22,6 @@
 				<img class="card-img-top" src="img/superfood.jpg" alt="Card image cap">
 				<div class="card-body">
 					<h5 class="card-title">Generic Salad</h5>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star checked"></span>
-					<span class="fa fa-star"></span>
-					<span class="fa fa-star"></span>
 					<p class="card-text">Upload an image of your recipe here.</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
