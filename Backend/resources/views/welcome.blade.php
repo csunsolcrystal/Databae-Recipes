0cab510f7ef38ccb04429d9aa081e026683675e7
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-09 14:12:15 -0800

    Update welcome.blade.php

--- Backend/resources/views/welcome.blade.php
+++ Backend/resources/views/welcome.blade.php
@@ -43,12 +43,5 @@
 	</div>
 </div>
 <!-- Bottom Content	-->
-<div class="container" style="margin-top: 50px">
-	<div class="card-body">
-		<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
-		<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
-		<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
-		<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
-	</div>
-</div>
+@include('recipes.index')
 @endsection
