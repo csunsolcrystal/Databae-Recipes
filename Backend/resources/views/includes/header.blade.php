d3d0af33fd87cb6016514df1657edf6c4e388e28
Sol Crystal <sol@ip-172-31-11-238.us-west-1.compute.internal>
2019-03-10 06:32:44 +0000

    possibly fixed the upload button size

--- Backend/resources/views/includes/header.blade.php
+++ Backend/resources/views/includes/header.blade.php
@@ -39,7 +39,7 @@
 				@endif
 			</div>
 			@else
-			<a href="/uploadrecipes" class="btn btn-secondary navbar-btn login-btn" style="margin-right: 50px">Upload a Recipe</a>
+			<a href="/uploadrecipes" class="btn btn-secondary navbar-btn login-btn" style="margin-right: 50px;height: 50%;">Upload a Recipe</a>
 			<li class="nav-item dropdown">
 				<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 					<img class="rounded-circle" width="30px" height="30px" src="/storage/avatars/{{ Auth::user()->avatar }}" /> {{ Auth::user()->username }} <span class="caret"></span>
