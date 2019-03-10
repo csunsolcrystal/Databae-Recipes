bc02f9196259429278126dfea3088afd08479c11
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-09 16:06:15 -0800

    Update index.blade.php

--- Backend/resources/views/recipes/index.blade.php
+++ Backend/resources/views/recipes/index.blade.php
@@ -1,27 +1,19 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="row">
-	<div class="card-group">
-		<div class="col">
-			@foreach($recipes as $recipe)
-			<a href='/recipes/{{$recipe->id}}'>
-				<div class="card" style="width: 18rem;">
-					<img src="/storage/recipes/{{ $recipe->picture }}" class="card-img-top" alt="{{ $recipe->title }}">
-					<div class="card-body">
-						<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
-						<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
-						<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
-						<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
-					</div>
-				</div>
-				@if ($loop->iteration % 2 == 0)
+@section('bottomcontent')
+<div class="card-group">
+	@foreach($recipes as $recipe)
+	<a href='/recipes/{{$recipe->id}}'>
+		<div class="card">
+			<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
+			<div class="card-body">
+				<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
+				<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
+				<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
+				<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
+			</div>
 		</div>
-		<div class="row">
-			@endif
-		</div></a>
-	</div>
-</div>
-</div>
-@endforeach
-@endsection
+		
+		@endforeach
+</div></a>
+@endsection;
