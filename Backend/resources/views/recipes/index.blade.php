8a6a827c7eb8d517b64f9a29338f70421ba26e6f
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-09 16:09:48 -0800

    Update index.blade.php

--- Backend/resources/views/recipes/index.blade.php
+++ Backend/resources/views/recipes/index.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
 @section('bottomcontent')
-<div class="card-group">
-	@foreach($recipes as $recipe)
-	<a href='/recipes/{{$recipe->id}}'>
-		<div class="card">
-			<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
-			<div class="card-body">
-				<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
-				<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
-				<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
-				<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
+<div class="row">
+	<div class="card-group">
+		@foreach($recipes as $recipe)
+		<a href='/recipes/{{$recipe->id}}'>
+			<div class="card">
+				<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
+				<div class="card-body">
+					<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
+					<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
+					<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
+					<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
+				</div>
+				@if ($loop->iteration % 2 == 0)
 			</div>
-		</div>
-		
-		@endforeach
+			<div class="row">
+				@endif
+			</div>
+			@endforeach
+	</div>
 </div></a>
 @endsection;
