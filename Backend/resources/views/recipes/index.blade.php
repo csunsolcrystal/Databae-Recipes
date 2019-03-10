fa436feddd0d239a6202aeb2409debe85fc887d5
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-09 16:19:31 -0800

    Update index.blade.php

--- Backend/resources/views/recipes/index.blade.php
+++ Backend/resources/views/recipes/index.blade.php
@@ -3,22 +3,22 @@
 @section('bottomcontent')
 @foreach ($recipes->chunk(3) as $recipe)
 <div class="row">
-<div class="card-group">
-	@foreach($recipes as $recipe)
-	<a href='/recipes/{{$recipe->id}}'>
-		<div class="card">
-			<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
-			<div class="card-body">
-				<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
-				<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
-				<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
-				<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
+	<div class="card-group">
+		@foreach($recipes as $recipe)
+		<a href='/recipes/{{$recipe->id}}'>
+			<div class="card">
+				<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
+				<div class="card-body">
+					<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
+					<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
+					<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
+					<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
+				</div>
 			</div>
-		</div>
-		
-		@endforeach
-</div></a>
-</div>
+
+			@endforeach
+		</a>
+	</div>
 </div>
 @endforeach
 @endsection;
