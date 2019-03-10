66ab3095c8ad6d53f550f3649ec681b203af497c
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-09 17:32:43 -0800

    Update index.blade.php

--- Backend/resources/views/recipes/index.blade.php
+++ Backend/resources/views/recipes/index.blade.php
@@ -7,29 +7,26 @@ $numofCols = 4;
 $rowCount = 0;
 @endphp
 
-<div class="row">
-<!--	<div class="card-group">-->
-		@foreach($recipes as $recipe)
-		<div class="card">
+	<div class="card-group">
+	@foreach($recipes as $recipe)
+	<div class="card">
 		<a href='/recipes/{{$recipe->id}}'>
-				<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
-				<div class="card-body">
-					<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
-					<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
-					<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
-					<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
-				</div>
+			<img class="card-img-top" width="369px" height="247px" src="/storage/recipes/{{ $recipe->picture }}" alt="{{ $recipe->title }}">
+			<div class="card-body">
+				<h5 class="card-title"><a href='/recipes/{{$recipe->id}}'>{{ $recipe->title }}</a></h5>
+				<p class="card-text">{{ str_limit($recipe->body, $limit = 150, $end = '...') }}</p>
+				<p class="card-text"><small class="text-muted">{{ $recipe->replies_count }} {{ str_plural('comment', $recipe->replies_count) }}</small></p>
+				<p class="card-text"><small class="text-muted">Last updated {{ $recipe->updated_at->diffForHumans() }}</small></p>
 			</div>
 		</a>
-<!--	</div>-->
-@php 
-$rowCount++;
-@endphp
-@if($rowCount % $numofCols == 0) 
-@php 
- echo 'div class="row">'
-@endphp
-</div>
-@endif
-@endforeach
+	</div>
+	@php
+	$rowCount++;
+	@endphp
+	@if($rowCount % $numofCols == 0)
+	@php
+	echo 'div class="card-group">'
+	@endphp
+	@endif
+	@endforeach
 @endsection
