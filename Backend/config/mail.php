1227f0e582e66f2cee5a7cb9cc481089046c9142
Sol Crystal <sol@ip-172-31-11-238.us-west-1.compute.internal>
2019-03-11 10:39:57 +0000

    no change

--- Backend/config/mail.php
+++ Backend/config/mail.php
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'no-reply@mail.databaerecipes.com'),
-        'name' => env('MAIL_FROM_NAME', 'No-Reply'),
+        'name' => env('MAIL_FROM_NAME', 'DataBae Solutions LLC'),
     ],
 
     /*
