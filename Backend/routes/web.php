98902458c63bd6da7792f8538d875c0359c4fd5f
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-03-16 16:14:10 -0700

    Update web.php

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -40,9 +40,11 @@ Route::group(['as' => 'user.'], function() {
 }
 */
 
+/*
 Route::group(['as' => 'user.'], function() {
     Route::any('/profile/{users}', [
         'as'    => 'profile',
         'uses'  => 'UserController@viewProfile'
     ]);
 }
+*/
