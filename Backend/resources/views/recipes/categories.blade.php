a8259300c04dc16a44fd92bd17e12e2362b9cf7b
Sol Crystal <sol@ip-172-31-11-238.us-west-1.compute.internal>
2019-03-22 22:43:52 +0000

    left something out

--- Backend/resources/views/recipes/categories.blade.php
+++ Backend/resources/views/recipes/categories.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<body style="background: url(img/background-board-chillies-1435895.jpg);margin: 0;background-position: center;background-repeat: no-repeat;background-size: cover;" >
+<body style="background: url(/img/background-board-chillies-1435895.jpg);margin: 0;background-position: center;background-repeat: no-repeat;background-size: cover;" >
 <div class="border rounded-0 bg-light shadow container my-4">
     <div class="py-3">
       <div class="container">
