0dc10ae2c2a3f7dbfcdfd12eeb79b1c36fde6ae8
Sol Crystal <sol@ip-172-31-11-238.us-west-1.compute.internal>
2019-03-30 21:45:31 +0000

    added other categories links

--- Backend/resources/views/recipes/categories.blade.php
+++ Backend/resources/views/recipes/categories.blade.php
@@ -59,7 +59,7 @@
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <a href="#" class="btn btn-primary">Read recipe</a>
               </div>
             </div>
-          </div>
+			</div>
           <div class="col-md-4">
             <div class="card"> <img class="card-img-top" src="https://static.pingendo.com/cover-moon.svg" alt="Card image cap">
               <div class="card-body">
@@ -78,11 +78,11 @@
             <h3 class="my-3"><b>Breakfast</b></h3>
             <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
           </a>
-          <a href="" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/beverages-brunch-cocktail-5317.jpg" width="348px" height="174px">
+          <a href="/recipes/categories/lunch" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/beverages-brunch-cocktail-5317.jpg" width="348px" height="174px">
             <h3 class="my-3"> <b>Lunch</b></h3>
             <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend.</p>
           </a>
-          <a href="" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/asparagus-barbecue-bbq-675951.jpg" width="348px" height="174px">
+          <a href="/recipes/categories/dinner" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/asparagus-barbecue-bbq-675951.jpg" width="348px" height="174px">
             <h3 class="my-3"> <b>Dinner</b></h3>
             <p>So absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke.</p>
           </a>
@@ -92,15 +92,15 @@
     <div class="py-5">
       <div class="container">
         <div class="row">
-          <a href="" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/berries-blueberries-cake-1126359.jpg" width="348px" height="174px">
+          <a href="/recipes/categories/dessert" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/berries-blueberries-cake-1126359.jpg" width="348px" height="174px">
             <h3 class="my-3"><b>Dessert</b></h3>
             <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
           </a>
-          <a href="" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/appetizers-biscuits-buffet-37922.jpg" width="348px" height="174px">
+          <a href="/recipes/categories/snacks" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/appetizers-biscuits-buffet-37922.jpg" width="348px" height="174px">
             <h3 class="my-3"> <b>Appetizers/Snacks</b></h3>
             <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend.</p>
           </a>
-          <a href="" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/beverages-cold-color-drinks-1643324.jpg" width="348px" height="174px">
+          <a href="/recipes/categories/drinks" class="col-md-4 col-6 p-3" style="max-width: 369px;color: inherit; text-decoration: inherit;z-index: 1;"> <img class="d-block rounded" src="/img/beverages-cold-color-drinks-1643324.jpg" width="348px" height="174px">
             <h3 class="my-3" contenteditable="true"> <b>Drinks</b></h3>
             <p>So absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke.</p>
           </a>
