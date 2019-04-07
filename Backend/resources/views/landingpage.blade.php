dd9f316d013a5d0532228e0e011dbda43d4f94d7
root <root@ip-172-31-11-238.us-west-1.compute.internal>
2019-04-07 00:13:43 +0000

    webpage

--- Backend/resources/views/landingpage.blade.php
+++ Backend/resources/views/landingpage.blade.php
@@ -1,65 +1,81 @@
+@include('includes.head')
 <!DOCTYPE html>
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
 </head>
 
 <body>
-  <div class="py-5 text-center text-white shadow-lg" style="position: relative; overflow: hidden;"> <video autoplay="" loop="" muted="" plays-inline="" style="-webkit-font-smoothing: antialiased; -webkit-filter: brightness(.5); filter: brightness(.5); position: absolute; right: 0; top: 0; min-width:100%; z-index: -100;">
-      <source src="videos/Sauteed Broccoli And Baby Corn.mp4" type="video/mp4"> </video>
-    <div class="container py-5">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <h1 class="mb-4 display-3">Databae Recipes</h1>
-          <p class="lead mb-5">A website dedicated for people to trying out new recipes. People from around the world can post and share their personal recipes for people to enjoy!&nbsp;</p>
-          <a href="/home" class="btn btn-lg btn-primary mx-1">Take me there!</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="py-3 shadow-sm">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 p-4">
-          <h4><i class="fas fa-list-alt"></i>&nbsp;Categories</h4>
-          <p>Looking for what to eat for dinner? Breakfast? Lunch? Recipes are categorized making it a lot simplier to find the recipe for you to enjoy and make!</p>
-        </div>
-        <div class="col-md-6 p-4">
-          <h4> <b><i class="fas fa-thumbs-up"></i>&nbsp;Favoriting</b></h4>
-          <p>If you enjoyed a recipe that you made or read a recipe that you want to save later and look at? Why don't you favorite that recipe to use for later?</p>
-        </div>
-        <div class="col-md-6 p-4">
-          <h4><i class="fas fa-comments"></i>&nbsp;Feedback/Comments</h4>
-          <p>Are you looking for a website to upload your personal recipe and share to the world and recieve feedback? Recipes on this website can be commented to provide feedback on your recipe!</p>
-        </div>
-        <div class="col-md-6 p-4">
-          <h4> <b><i class="fas fa-star checked"></i>&nbsp;Rating System</b></h4>
-          <p>Recipes on this website can be rated out of five stars. It has a unique calculation on determining what recipes stand out!</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="py-5 shadow-sm" style="" >
-    <div class="container">
-      <div class="row">
-        <div class="text-center mx-auto col-md-12">
-          <h1>Countless amount of Recipes</h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 p-3"> <img class="img-fluid d-block" src="img/beef-bread-buns-1633578.jpg"> </div>
-        <div class="col-md-4 p-3"> <img class="img-fluid d-block" src="img/baking-cheese-cooking-315755.jpg"> </div>
-        <div class="col-md-4 p-3"> <img class="img-fluid d-block" src="img/brownie-cake-chocolate-45202.jpg"> </div>
-      </div>
-    </div>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<div class="py-5 text-center text-white shadow-lg" style="position: relative; overflow: hidden;"> <video autoplay="" loop="" muted="" plays-inline="" style="-webkit-font-smoothing: antialiased; -webkit-filter: brightness(.5); filter: brightness(.5); position: absolute; right: 0; top: 0; min-width:100%; z-index: -100;">
+			<source src="videos/Sauteed Broccoli And Baby Corn.mp4" type="video/mp4"> </video>
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-lg-8 col-md-10 mx-auto">
+					<h1 class="mb-4 display-3">Databae Recipes</h1>
+					<p class="lead mb-5">Find and share recipes with others who have different levels of cooking experience.&nbsp;</p>
+					<a href="/home" class="btn btn-lg btn-primary mx-1">Take me there!</a>
+					<a href="/register" class="btn btn-lg btn-primary mx-1">Sign Up</a>
+				</div>
+				<div class="col-lg-8 col-md-10 mx-auto">
+					<form class="typeahead ml-auto mr-auto" role="search">
+						{{ csrf_field() }}
+						<div class="btn-group">
+							<div class="u-posRelative">
+								<input class="form-control search-input" name="q" type="search" autocomplete="off" placeholder="Search recipes..." aria-label="Search" style="width: 500px">
+							</div>
+							<div class="Typeahead-menu"></div>
+							<button class="btn btn-default" type="submit">
+								<img src="/img/search_icon.png" alt="Smiley face" height="20px" width="20px">
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="py-3 shadow-sm">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 p-4">
+					<h4><i class="fas fa-list-alt"></i>&nbsp;Categories</h4>
+					<p>Looking for what to eat for dinner? Breakfast? Lunch? Recipes are categorized making it a lot simplier to find the recipe for you to enjoy and make!</p>
+				</div>
+				<div class="col-md-6 p-4">
+					<h4> <b><i class="fas fa-thumbs-up"></i>&nbsp;Favoriting</b></h4>
+					<p>If you enjoyed a recipe that you made or read a recipe that you want to save later and look at? Why don't you favorite that recipe to use for later?</p>
+				</div>
+				<div class="col-md-6 p-4">
+					<h4><i class="fas fa-comments"></i>&nbsp;Feedback/Comments</h4>
+					<p>Are you looking for a website to upload your personal recipe and share to the world and recieve feedback? Recipes on this website can be commented to provide feedback on your recipe!</p>
+				</div>
+				<div class="col-md-6 p-4">
+					<h4> <b><i class="fas fa-star checked"></i>&nbsp;Rating System</b></h4>
+					<p>Recipes on this website can be rated out of five stars. It has a unique calculation on determining what recipes stand out!</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="py-5 shadow-sm" style="">
+		<div class="container">
+			<div class="row">
+				<div class="text-center mx-auto col-md-12">
+					<h1>Countless amount of Recipes</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 p-3"> <img class="img-fluid d-block" src="img/beef-bread-buns-1633578.jpg"> </div>
+				<div class="col-md-4 p-3"> <img class="img-fluid d-block" src="img/baking-cheese-cooking-315755.jpg"> </div>
+				<div class="col-md-4 p-3"> <img class="img-fluid d-block" src="img/brownie-cake-chocolate-45202.jpg"> </div>
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
