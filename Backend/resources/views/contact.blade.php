65d5f2c6fb2eac558ef747d49b63355b82a65314
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 16:12:38 -0700

    Update contact.blade.php

--- Backend/resources/views/contact.blade.php
+++ Backend/resources/views/contact.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 <div class="border rounded-0 bg-light shadow container my-4">
-	<div class="row text-center d-flex justify-content-center pt-5 mb-3">
-		<div class="col-md-2 mb-3">
+	<div class="row">
+		<div class="col-md-5">
 			<h1>Contact Us</h1>
 			<h2>Email</h2>
 			<p>support@mail.databaerecipes.com</p>
 			<h2>Phone</h2>
 			<p>555-555-5555</p>
 		</div>
-		<div class="col-md-2 mb-3">
+		<div class="col-md-2">
 			FAQs
 			<ul>
 				<li>yeet</li>
