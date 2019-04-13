f9485a067f0b56b25a8b372109345816a927ff6a
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 15:46:41 -0700

    Update footer.blade.php

--- Backend/resources/views/includes/footer.blade.php
+++ Backend/resources/views/includes/footer.blade.php
@@ -2,13 +2,13 @@
 	<footer id="sticky-footer" class="py-5 text-dark-50">
 		<div class="border rounded-0 bg-light shadow container my-4">
 			<ul style="display: inline">
-				<li><a href='/contact'>Contact Us</a></li>
-				<li><a href='/contact'>About Us</a></li>
-				<li><a href='/contact'>Terms of Use</a></li>
-				<li><a href='/contact'>Site Map</a></li>
-				<li><a href='/contact'>Support</a></li>
-				<li><a href='/contact'>Employment Opportunities</a></li>
-				<li><a href='/contact'>Advertise</a></li>
+				<li style="display: inline"><a href='/contact'>Contact Us</a></li>
+				<li style="display: inline"><a href='/contact'>About Us</a></li>
+				<li style="display: inline"><a href='/contact'>Terms of Use</a></li>
+				<li style="display: inline"><a href='/contact'>Site Map</a></li>
+				<li style="display: inline"><a href='/contact'>Support</a></li>
+				<li style="display: inline"><a href='/contact'>Employment Opportunities</a></li>
+				<li style="display: inline"><a href='/contact'>Advertise</a></li>
 			</ul>
 			<div class="container text-center">
 				<small>Copyright &#169 2018 by DataBae Solutions, LLC</small>
