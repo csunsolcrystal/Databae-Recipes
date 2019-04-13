ca4ca885e22a43da20ec54eacb43c2f6a85ea73a
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 15:04:54 -0700

    Add Contact us to Footer

--- Backend/resources/views/includes/footer.blade.php
+++ Backend/resources/views/includes/footer.blade.php
@@ -1,7 +1,10 @@
 <body class="d-flex flex-column">
 <footer id="sticky-footer" class="py-5 text-dark-50">
-    <div class="container text-center">
+   <div class="border rounded-0 bg-light shadow container my-4"> 
+		<p>Contact Us</p>
+	<div class="container text-center">
       <small>Copyright &#169 2018 by DataBae Solutions, LLC</small>
     </div>
+	</div>
   </footer>
 </body>
