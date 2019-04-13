dab2f8ab46525bb380d383d1d40d7f43c55e9770
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 15:45:23 -0700

    Update footer.blade.php

--- Backend/resources/views/includes/footer.blade.php
+++ Backend/resources/views/includes/footer.blade.php
@@ -1,18 +1,18 @@
 <body class="d-flex flex-column">
-<footer id="sticky-footer" class="py-5 text-dark-50">
-   <div class="border rounded-0 bg-light shadow container my-4"> 
-		<ul>
-		<li><a href='/contact'>Contact Us</a></li>
-		<li><a href='/contact'>About Us</a></li>
-		<li><a href='/contact'>Terms of Use</a></li>
-		<li><a href='/contact'>Site Map</a></li>
-		<li><a href='/contact'>Support</a></li>
-		<li><a href='/contact'>Employment Opportunities</a></li>
-		<li><a href='/contact'>Advertise</a></li>
+	<footer id="sticky-footer" class="py-5 text-dark-50">
+		<div class="border rounded-0 bg-light shadow container my-4">
+			<ul style="display: inline">
+				<li><a href='/contact'>Contact Us</a></li>
+				<li><a href='/contact'>About Us</a></li>
+				<li><a href='/contact'>Terms of Use</a></li>
+				<li><a href='/contact'>Site Map</a></li>
+				<li><a href='/contact'>Support</a></li>
+				<li><a href='/contact'>Employment Opportunities</a></li>
+				<li><a href='/contact'>Advertise</a></li>
 			</ul>
-	<div class="container text-center">
-      <small>Copyright &#169 2018 by DataBae Solutions, LLC</small>
-    </div>
-	</div>
-  </footer>
+			<div class="container text-center">
+				<small>Copyright &#169 2018 by DataBae Solutions, LLC</small>
+			</div>
+		</div>
+	</footer>
 </body>
