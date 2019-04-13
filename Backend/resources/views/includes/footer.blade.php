9e6ff35ce4cd595d86b59e3059a24eb1343d9af8
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 15:52:10 -0700

    Update footer.blade.php

--- Backend/resources/views/includes/footer.blade.php
+++ Backend/resources/views/includes/footer.blade.php
@@ -31,7 +31,7 @@
 			<!-- Grid column -->
 			<div class="col-md-2 mb-3">
 				<h6 class="text-uppercase font-weight-bold">
-					<a href="#!">About us</a>
+					<a href="/contact">About us</a>
 				</h6>
 			</div>
 			<!-- Grid column -->
@@ -39,7 +39,7 @@
 			<!-- Grid column -->
 			<div class="col-md-2 mb-3">
 				<h6 class="text-uppercase font-weight-bold">
-					<a href="#!">Products</a>
+					<a href="/contact">Site Map</a>
 				</h6>
 			</div>
 			<!-- Grid column -->
@@ -47,7 +47,7 @@
 			<!-- Grid column -->
 			<div class="col-md-2 mb-3">
 				<h6 class="text-uppercase font-weight-bold">
-					<a href="#!">Awards</a>
+					<a href="/contact">Other Links</a>
 				</h6>
 			</div>
 			<!-- Grid column -->
@@ -55,7 +55,7 @@
 			<!-- Grid column -->
 			<div class="col-md-2 mb-3">
 				<h6 class="text-uppercase font-weight-bold">
-					<a href="#!">Help</a>
+					<a href="/contact">Support</a>
 				</h6>
 			</div>
 			<!-- Grid column -->
@@ -63,7 +63,7 @@
 			<!-- Grid column -->
 			<div class="col-md-2 mb-3">
 				<h6 class="text-uppercase font-weight-bold">
-					<a href="#!">Contact</a>
+					<a href="/contact">Contact</a>
 				</h6>
 			</div>
 			<!-- Grid column -->
@@ -85,55 +85,13 @@
 
 		</div>
 		<!-- Grid row-->
-		<hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
-
-		<!-- Grid row-->
-		<div class="row pb-3">
-
-			<!-- Grid column -->
-			<div class="col-md-12">
-
-				<div class="mb-5 flex-center">
-
-					<!-- Facebook -->
-					<a class="fb-ic">
-						<i class="fab fa-facebook-f fa-lg white-text mr-4"> </i>
-					</a>
-					<!-- Twitter -->
-					<a class="tw-ic">
-						<i class="fab fa-twitter fa-lg white-text mr-4"> </i>
-					</a>
-					<!-- Google +-->
-					<a class="gplus-ic">
-						<i class="fab fa-google-plus-g fa-lg white-text mr-4"> </i>
-					</a>
-					<!--Linkedin -->
-					<a class="li-ic">
-						<i class="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
-					</a>
-					<!--Instagram-->
-					<a class="ins-ic">
-						<i class="fab fa-instagram fa-lg white-text mr-4"> </i>
-					</a>
-					<!--Pinterest-->
-					<a class="pin-ic">
-						<i class="fab fa-pinterest fa-lg white-text"> </i>
-					</a>
-
-				</div>
-
-			</div>
-			<!-- Grid column -->
-
-		</div>
-		<!-- Grid row-->
 
 	</div>
 	<!-- Footer Links -->
 
 	<!-- Copyright -->
-	<div class="footer-copyright text-center py-3">© 2018 Copyright:
-		<a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+	<div class="footer-copyright text-center py-3">© 2019 Copyright:
+		<a href="databaerecipes.com">databaerecipes.com</a>
 	</div>
 	<!-- Copyright -->
 
