ec63163478e6ba4d3395626d0be1986a2699fe68
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 15:09:49 -0700

    Update footer.blade.php

--- Backend/resources/views/includes/footer.blade.php
+++ Backend/resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <body class="d-flex flex-column">
 <footer id="sticky-footer" class="py-5 text-dark-50">
    <div class="border rounded-0 bg-light shadow container my-4"> 
-		<p>Contact Us</p>
+		<p>Contact Us Yeet</p>
 	<div class="container text-center">
       <small>Copyright &#169 2018 by DataBae Solutions, LLC</small>
     </div>
