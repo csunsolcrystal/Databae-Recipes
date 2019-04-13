996d1c038a4ab7b113984d49914dd97a7802f556
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 16:19:55 -0700

    Update contact.blade.php

--- Backend/resources/views/contact.blade.php
+++ Backend/resources/views/contact.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-	<div class="col-md-8 mb-3">
-		<div class="border rounded-0 bg-light shadow container">
+<div class="border rounded-0 bg-light shadow container my-4">
+	<div class="row">
+		<div class="col-xs-6 col-lg-8">
 			<h1>Contact Us</h1>
 			<h2>Email</h2>
 			<p>support@mail.databaerecipes.com</p>
 			<h2>Phone</h2>
 			<p>555-555-5555</p>
 		</div>
-		<div class="col-md-4 mb-3">
-			<div class="border rounded-0 bg-light shadow container">
-				FAQs
-				<ul>
-					<li>yeet</li>
-				</ul>
-			</div>
+		<div class="col-xs-6 col-lg-4">
+			FAQs
+			<ul>
+				<li>yeet</li>
+			</ul>
 		</div>
 	</div>
 </div>
