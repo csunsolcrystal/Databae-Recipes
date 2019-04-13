82577cc93832ab34c19b2ba55eee881c55fd9fac
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 15:43:44 -0700

    Update footer.blade.php

--- Backend/resources/views/includes/footer.blade.php
+++ Backend/resources/views/includes/footer.blade.php
@@ -1,13 +1,15 @@
 <body class="d-flex flex-column">
 <footer id="sticky-footer" class="py-5 text-dark-50">
    <div class="border rounded-0 bg-light shadow container my-4"> 
-		<a href='/contact'>Contact Us</a>
-		<a href='/contact'>About Us</a>
-		<a href='/contact'>Terms of Use</a>
-		<a href='/contact'>Site Map</a>
-		<a href='/contact'>Support</a>
-		<a href='/contact'>Employment Opportunities</a>
-		<a href='/contact'>Advertise</a>
+		<ul>
+		<li><a href='/contact'>Contact Us</a></li>
+		<li><a href='/contact'>About Us</a></li>
+		<li><a href='/contact'>Terms of Use</a></li>
+		<li><a href='/contact'>Site Map</a></li>
+		<li><a href='/contact'>Support</a></li>
+		<li><a href='/contact'>Employment Opportunities</a></li>
+		<li><a href='/contact'>Advertise</a></li>
+			</ul>
 	<div class="container text-center">
       <small>Copyright &#169 2018 by DataBae Solutions, LLC</small>
     </div>
