62828c3b6c9ff20b36dabde705146decb914cc54
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-13 16:25:01 -0700

    Update contact.blade.php

--- Backend/resources/views/contact.blade.php
+++ Backend/resources/views/contact.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="border rounded-0 bg-light shadow container my-4">
-	<div class="row">
+	<div class="row my-4">
 		<div class="col-xs-6 col-lg-8">
 			<h1>Contact Us</h1>
 			<h2>Email</h2>
@@ -11,12 +11,14 @@
 			<p>555-555-5555</p>
 		</div>
 		<div class="col-xs-6 col-lg-4">
-		<div class="border rounded-0 bg-light container my-4">
-			FAQs
-			<ul>
-				<li>yeet</li>
-			</ul>
-		</div>
+			<div class="border rounded-0 bg-light container my-4">
+				<h2>FAQs</h2>
+				<ul>
+					<li>Why is my pee pee hard?</li>
+					<li>My recipe isn't uploading.</li>
+					<li>I'm getting an error when I log in.</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
