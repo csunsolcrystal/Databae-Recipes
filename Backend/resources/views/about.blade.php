499ae3a0cf1e882fa222e28fb49a9ea23ee88140
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-20 16:21:01 -0700

    About Us Page Updates

--- Backend/resources/views/about.blade.php
+++ Backend/resources/views/about.blade.php
@@ -4,16 +4,51 @@
 <div class="border rounded-0 bg-light shadow container my-4">
 	<div class="container mt-4">
 		<div class="card-group mb-4">
-			<img class="card-img-top" width="369px" height="247px" src="/img/Tableman2.jpg" alt="Ryan">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Tableman2.jpg" alt="Ryan Johnston">
+			<div class="card-body">
+				<h5 class="card-title">Jeffrey Garcia</h5>
+				<p class="card-text">Project Manager</p>
+			</div>
+		</div>
+		<div class="card-group mb-4">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Sol.jpg" alt="Sol Crystal">
+			<div class="card-body">
+				<h5 class="card-title">Sol Crystal</h5>
+				<p class="card-text">Dev Ops</p>
+			</div>
+		</div>
+		<div class="card-group mb-4">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Mark.jpg" alt="Mark Requiereme">
+			<div class="card-body">
+				<h5 class="card-title">Mark Requiereme</h5>
+				<p class="card-text">He is gay.</p>
+			</div>
+		</div>
+		<div class="card-group mb-4">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Tableman2.jpg" alt="Aaron Park">
+			<div class="card-body">
+				<h5 class="card-title">Aaron Park</h5>
+				<p class="card-text">He is gay.</p>
+			</div>
+		</div>
+		<div class="card-group mb-4">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Tableman2.jpg" alt="Greg">
+			<div class="card-body">
+				<h5 class="card-title">Greg</h5>
+				<p class="card-text">He is gay.</p>
+			</div>
+		</div>
+		<div class="card-group mb-4">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Tableman2.jpg" alt="Ryan Johnston">
 			<div class="card-body">
 				<h5 class="card-title">Ryan Johnston</h5>
 				<p class="card-text">He is gay.</p>
 			</div>
 		</div>
 		<div class="card-group mb-4">
-			<img class="card-img-top" width="369px" height="247px" src="/img/Tableman2.jpg" alt="Ryan">
+			<img class="card-img-top" width="369px" height="247px" src="/img/Nelson.jpg" alt="Nelson Figueroa">
 			<div class="card-body">
-				<h5 class="card-title">Mark Requierme</h5>
+				<h5 class="card-title">Nelson Figueroa</h5>
 				<p class="card-text">He is gay.</p>
 			</div>
 		</div>
