93ee8c4f699ea6e43c5829f37faf09282dbac761
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-20 17:35:12 -0700

    Update support.blade.php

--- Backend/resources/views/support.blade.php
+++ Backend/resources/views/support.blade.php
@@ -1,38 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-{!! Form::Open(['url' => 'sendmail']) !!}
-
-<div class="col_half">
-   {!! Form::label('name', 'Name: ' ) !!}
-   {!! Form::text('name', null, ['class' => 'form-control', 'required']) !!}
-</div>
-
-<div class="col_half col_last">
-   {!! Form::label('email', 'E-Mail: ' ) !!}
-   {!! Form::email('email', null, ['class' => 'form-control', 'required']) !!}
-</div>
-
-<div class="clear"></div>
-
-<div class="col_full col_last">
-   {!! Form::label('subject', 'Subject: ' ) !!}
-   {!! Form::text('subject', null, ['class' => 'form-control', 'required']) !!}
-</div>
-
-<div class="clear"></div>
-
-<div class="col_full">
-    {!! Form::label('bodymessage', 'Message: ' ) !!}
-    {!! Form::textarea('bodymessage', null, ['class' => 'form-control', 'required', 'size' => '30x6']) !!}
-</div>
-
-
-<div class="col_full">
-     {!! Form::submit('Send') !!}
-</div>
-{!! Form::close() !!}
-<!--
 <div class="border rounded-0 bg-light shadow container my-4">
 	<div class="row my-4">
 		<div class="col-xs-6 col-lg-8">
@@ -65,5 +33,4 @@
 		</div>
 	</div>
 </div>
--->
 @endsection
