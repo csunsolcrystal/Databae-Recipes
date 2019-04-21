85af20b54803c3113a6825a60e26f4a7a4cf9703
Moniushko <44934043+Moniushko@users.noreply.github.com>
2019-04-20 17:21:43 -0700

    a

--- Backend/app/Http/Controllers/PagesController.php
+++ Backend/app/Http/Controllers/PagesController.php
@@ -42,6 +42,37 @@ return view('welcome', compact('recipes'));
     	return view('recipes.index', compact('recipes'));
     }
 
+public function sendMail(Request $request) {
+    //dd($request->all());
+
+    $validator = \Validator::make($request->all(), [
+                                'name' => 'required|max:255',
+                                'email' => 'required|email|max:255',
+                                'subject' => 'required',
+                                'bodymessage' => 'required']
+    );
+
+        if ($validator->fails()) {
+            return redirect('contact')->withInput()->withErrors($validator);
+        }
+
+
+    $name = $request->name;
+    $email = $request->email;
+    $title = $request->subject;
+    $content = $request->bodymessage;
+
+
+        \Mail::send('emails.visitor_email', ['name' => $name, 'email' => $email, 'title' => $title, 'content' => $content], function ($message) {
+
+            $message->to('your.email@gmail.com')->subject('Subject of the message!');
+        });
+
+
+    return redirect('contact')->with('status', 'You have successfully sent an email to the admin!');
+
+}
+	
 public function landing() {
 	return view('landingpage');
 }
--- Backend/resources/views/support.blade.php
+++ Backend/resources/views/support.blade.php
@@ -1,6 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
+{!! Form::Open(['url' => 'sendmail']) !!}
+
+<div class="col_half">
+   {!! Form::label('name', 'Name: ' ) !!}
+   {!! Form::text('name', null, ['class' => 'form-control', 'required']) !!}
+</div>
+
+<div class="col_half col_last">
+   {!! Form::label('email', 'E-Mail: ' ) !!}
+   {!! Form::email('email', null, ['class' => 'form-control', 'required']) !!}
+</div>
+
+<div class="clear"></div>
+
+<div class="col_full col_last">
+   {!! Form::label('subject', 'Subject: ' ) !!}
+   {!! Form::text('subject', null, ['class' => 'form-control', 'required']) !!}
+</div>
+
+<div class="clear"></div>
+
+<div class="col_full">
+    {!! Form::label('bodymessage', 'Message: ' ) !!}
+    {!! Form::textarea('bodymessage', null, ['class' => 'form-control', 'required', 'size' => '30x6']) !!}
+</div>
+
+
+<div class="col_full">
+     {!! Form::submit('Send') !!}
+</div>
+{!! Form::close() !!}
+<!--
 <div class="border rounded-0 bg-light shadow container my-4">
 	<div class="row my-4">
 		<div class="col-xs-6 col-lg-8">
@@ -24,7 +56,7 @@
 					<label for="textArea">Describe your problem here...</label>
 					<textarea class="form-control mb-4" name="Describe your problem here..." id="ticketIssue" rows="3"></textarea>
 					<label for="email">Email Address</label>
-					<input type="email" name="title" class="form-control mb-2" id="email">
+					<input type="email" name="title" class="form-control mb-4" id="email">
 				</div>
 				<button type="submit" class="btn btn-primary mt-4 mb-4">
 					Submit
@@ -33,4 +65,5 @@
 		</div>
 	</div>
 </div>
+-->
 @endsection
